3432e3410de14b3938b7ffc3f40907d9913f4112
Drew Rice <drice@capson.com>
2017-02-28 12:48:32 -0600

    string type checking and conversion

--- polling.py
+++ polling.py
@@ -1,25 +1,39 @@
+######################################################################
+#                                                                    #
+#                   Basic Python polling object                      #
+#                                                                    #
+######################################################################
+
 class Poll(object):
     def __init__(self, poll_options):
         '''
         INPUT:  List of voting options
 
-        TODO: limit the number of poll options
+        Initializes the poll. Converts all data to strings. Raises ValueError if no options provided.
+
+        TODO: limit the number of poll_options, length of option strings
         '''
+        self.poll_options = [str(k) for k in poll_options]
         if poll_options:
-            self.poll_options = list(poll_options)
+            self.poll_options = list(self.poll_options)
             self.poll_options_dict = {k:0 for k in self.poll_options}
+        else:
+            raise ValueError('No voting options provided.')
 
     def vote(self, selection):
         '''
         INPUT: option to vote for
 
-        Adjusts the values in the options dictionary
+        Type checks the selection, converts to string. Adjusts the values in the options dictionary.
+        If the option doesn't exist, fail silently.
         '''
+        if type(selection) != str:
+            selection = str(selection)
         if selection in self.poll_options:
             self.poll_options_dict[selection] += 1
 
 if __name__ == '__main__':
-    poll = Poll(['option1','option2','42'])
+    poll = Poll(['bap','boop','42'])
     poll.vote('42')
     poll.vote('42')
     print poll.poll_options_dict
