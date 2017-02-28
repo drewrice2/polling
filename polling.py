fd8ab150c5ba294645af80f749b59fdff6167318
Drew Rice <drice@capson.com>
2017-02-28 17:00:56 -0600

    included some updates, adjusted type checking and error handling

--- polling.py
+++ polling.py
@@ -5,13 +5,15 @@
 ######################################################################
 
 class Poll(object):
+
     def __init__(self, poll_options):
         '''
         INPUT:  List of voting options
 
-        Initializes the poll. Converts all data to strings. Raises ValueError if no options provided.
+        Initializes the poll.
+        Converts the list items to strings. Raises ValueError if no options provided.
 
-        TODO: limit the number of poll_options, length of option strings
+        TODO: limit both the number of poll_options, length of option strings
         '''
         self.poll_options = [str(k) for k in poll_options]
         if poll_options:
@@ -24,6 +26,7 @@ class Poll(object):
         '''
         INPUT: option to vote for
 
+        Votes for the given selection.
         Type checks the selection, converts to string. Adjusts the values in the options dictionary.
         If the option doesn't exist, fail silently.
         '''
@@ -33,7 +36,7 @@ class Poll(object):
             self.poll_options_dict[selection] += 1
 
 if __name__ == '__main__':
-    poll = Poll(['bap','boop','42'])
+    poll = Poll(['bap','boop','42','blah-blah-blah'])
     poll.vote('42')
     poll.vote('42')
     print poll.poll_options_dict
